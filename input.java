b770b8a2d822fce15d292f89f63c2a3b5540094f
Andrey Kutalev <akutalev@gmail.com>
2013-09-16 16:37:35 +0700

    Some refactoring.

--- input.java
+++ input.java
@@ -1,3 +1,3 @@
 asjdk hak sjd(sdfs          dfdf){sdfsd
   fsdf;for(sdfsd;sdf
-    sfd;sdfs){aaa;s(ss;} else {sdfsdf;	sdfsdf;		   }sdf		sdf;sdfsdf;}
+    sfd;sdfs){aaa;sss;} else {sdfsdf;	sdfsdf;		   }sdf		sdf;sdfsdf;}
